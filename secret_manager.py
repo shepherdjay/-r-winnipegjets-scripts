33d4572ca90ea122fa5cd9a0d6aa36cbc9e0abd1
Kyle Brown <KyleBrownCS@users.noreply.github.com>
2017-11-20 21:00:59 -0600

    refactor secret retreival

--- secret_manager.py
+++ secret_manager.py
@@ -10,14 +10,23 @@ class SecretManager():
         with open(APPLICATION_SECRET_FILE) as json_data:
             self.secrets = json.load(json_data)
 
+    def _get_value(self, team, obj, detail=None):
+        if detail:
+            return self.secrets.get(str(team)).get(obj).get(detail)
+        return self.secrets.get(str(team)).get(obj)
+
     def get_team_contacts(self, team):
         """takes a number and returns the redditors that are associated to that folder."""
-        return self.secrets.get(str(team)).get('admin')
+        return self._get_value(team, 'admin') 
 
     def get_reddit_name(self, team):
         """takes a number and returns the redditors that are associated to that folder."""
-        return self.secrets.get(str(team)).get('reddit')
+        return self._get_value(team, 'reddit')
 
     def get_teams_parent_folder(self, team):
+        """take the teamid and return the fold that this team has all their data stored in"""
+        return self._get_value(team, 'folder')
 
-        return self.secrets.get(str(team)).get('folder')
+    def get_previous_winners(self, team):
+        """get the previous winners details"""
+        return self._get_value(team, "details", detail='winners')
